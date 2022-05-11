5a7901984df168c186ef19a50ac7aca3c1c33422
agui <55447192+GuiWonder@users.noreply.github.com>
2022-05-10 21:06:43 -0500

    Update FormMain.cs

--- CharToUnicode/CharToUnicode/FormMain.cs
+++ CharToUnicode/CharToUnicode/FormMain.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Windows.Forms;
 
 namespace CharToUnicode
@@ -8,26 +8,38 @@ namespace CharToUnicode
         public FormMain()
         {
             InitializeComponent();
+            textBoxChar.TextChanged += TextBoxChar_TextChanged;
+            checkBoxReturn.CheckedChanged += TextBoxChar_TextChanged;
+            checkBoxSpace.CheckedChanged += TextBoxChar_TextChanged;
+            checkBoxTab.CheckedChanged += TextBoxChar_TextChanged;
+            checkBoxUppercase.CheckedChanged += TextBoxChar_TextChanged;
+            textBoxPrefix.TextChanged += TextBoxChar_TextChanged;
         }
 
-        private void ButtonConvert_Click(object sender, EventArgs e)
+        private void TextBoxChar_TextChanged(object sender, EventArgs e)
+        {
+            textBoxUnicode.Text = CharToUnicode(textBoxChar.Text);
+        }
+
+        private string CharToUnicode(string text)
         {
             string result = "";
-            System.Globalization.TextElementEnumerator t = System.Globalization.StringInfo.GetTextElementEnumerator(textBoxChar.Text);
+            System.Globalization.TextElementEnumerator t = System.Globalization.StringInfo.GetTextElementEnumerator(text);
             while (t.MoveNext())
             {
                 string s = t.GetTextElement();
-                if ((!checkBoxReturn.Checked && (s == "\r" || s == "\n")) || (!checkBoxSpace.Checked && s == " ") || (!checkBoxTab.Checked && s == "\t"))
+                if ((checkBoxReturn.Checked && (s == "\r" || s == "\n")) || (checkBoxSpace.Checked && s == " ") || (checkBoxTab.Checked && s == "\t"))
                 {
                     result += s;
                 }
                 else
                 {
                     uint unicode = s.Length == 1 ? s[0] : 0x10000 + ((s[0] & 0x3ffu) << 10) + (s[1] & 0x3ffu);
-                    result += "u" + Convert.ToString(unicode, 16);
+                    string hex = Convert.ToString(unicode, 16);
+                    result += checkBoxUppercase.Checked ? textBoxPrefix.Text + hex.ToUpper() : textBoxPrefix.Text + hex;
                 }
             }
-            textBoxUnicode.Text = result;
+            return result;
         }
     }
 }
